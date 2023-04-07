46736cd3fc67cec7c56c29879778494a62d8c12d
hookor <115582699+hookor@users.noreply.github.com>
2023-04-07 22:43:18 +0900

    CSS_rough_measure

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log('Hello!');
+//  ITEMS
+// const alert = document.querySelector('.alert');
+// const form = document.querySelector('.grocery-form');
+// const grocery = document.getElementById('grocery') //input
+// const submitBtn = document.querySelector('.submit-btn');
+// const container = document.querySelector('.grocery-container');
+// const list = document.querySelector('.grocery-list');
+// const clearBtn = document.querySelector('.clear-btn');
+
+// edit option
+// let editElement;
+// let editFocus = false;
+// let editID = "";
+
+
+// EVENT LISTENERS
+// submit form
+// form.addEventListener('submit', addItem)
+// clear list
+//
+// initial display
+
+// FUNCTIONS
+// add item
+// add eventlistener to both buttons 
+// append child
+// display alert
+// show
+// function addItem(e){
+//   e.preventDefault();
+//   const value = grocery.value;
+  // 1. typed value & not focused 2. typed value & focused 3. untyped value & unfocused(default)
+  // (value && !editFocus) ? console.log('add item to the list') :
+  // (value && editFocus ) ? console.log("typing") : console.log("empty value")
+
+  
+
+  // console.log(grocery.value);
+    // make list-item{} with the value{grocery.value} into the list{list.innerHTML}
+
+// }
+
+
+//LOCAL STORAGE
+
+
+
+
+//SETUP
